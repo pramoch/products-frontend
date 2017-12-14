09f0bc2aaeba329e8b0da56d12e402bea424d1ab
Pramoch Chulapong <pramoch.chulapong@thomsonreuters.com>
2017-12-14 13:28:25 +0700

    Add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
